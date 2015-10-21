ad4498b3ee6a2a54765db0d07bc57ea0cc387cf8
Fabian Monsalve <fgmonsalveb@unal.edu.co>
2015-10-21 00:00:37 -0500

    Correccion File y creacion Forum

--- domyCons/grails-app/domain/domycons/File.groovy
+++ domyCons/grails-app/domain/domycons/File.groovy
@@ -2,6 +2,28 @@ package domycons
 
 class File {
 
+    String fileType //  debe verificarse que tenga al menos un “/” y mínimo un caracter antes y un caracter después de dicho símbolo
+    Byte[] content // El atributo “content” contiene los bytes del archivo en cuestión
+    double size // El tamaño (“size”) de un archivo representa el tamaño del archivo en BYTES y  debe ser máximo de 10 MB (MegaBytes)
+
+
     static constraints = {
+        fileType validator: {
+            if (it.size()>= 3){
+                idx = it.indexOf("/")
+                if(idx < 1 && idx >= it.size()-1){
+                    return ['error tipo de file ']
+                }
+            }else{
+                return ['error tipo de file ']
+            }
+        }
+
+        content nullable: false
+        size validator: {
+            if(size > 1048576){
+                return ["tamaño mayor a 10 MB"]
+            }
+        }
     }
 }
---
+++ domyCons/grails-app/domain/domycons/Forum.groovy
@@ -0,0 +1,18 @@
+package domycons
+
+class Forum {
+
+    String name //El nombre del foro debe tener una longitud mínima de 3 caracteres, una longitud máxima de 20 caracteres y debe ser único
+    Date dateCreated // La fecha de creación de un foro debe ser futura respecto a la fecha actual
+    String category //  La categoría de un foro debe tener una longitud mínima de 3 caracteres y una longitud máxima de 15 caracteres
+
+
+    static constraints = {
+        name size: 3..20, unique: true
+        dateCreated validator:{
+            Date dateobj = new Date()
+            if(!it.after(dateobj)) return ['la fecha es pasada a la creacion del post']
+        }
+        category size: 3..15
+    }
+}
