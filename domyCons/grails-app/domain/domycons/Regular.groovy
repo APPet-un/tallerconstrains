2560c2a791124a88e5cad1f66f1dc61ce7c5294b
Sebastian <smunozca@unal.edu.co>
2015-10-20 23:18:04 -0500

    Clase Regular --Sebas

---
+++ domyCons/grails-app/domain/domycons/Regular.groovy
@@ -0,0 +1,19 @@
+package domycons
+
+class Regular {
+
+	int postViews
+	int strikesNumber
+	int starsNumber
+
+    static constraints = {
+	
+	postViews (blank: false)		//Numero de post no puede ser nulo
+	strikesNumber (blank: false)	//Numero de strikes no puede ser nulo
+	starsNumber (blank: false)		//Numero de estrellas no puede ser nulo
+	
+	postViews (min: 0)				//El numero de post inicia en 0 y no tiene limite superior
+	strikesNumber (range: 0..3)		//Se pueden tener de 0 a 3 strikes
+	starsNumber (range: 0..5)		//Se pueden tener de 0 a 5 estrellas
+    }
+}
