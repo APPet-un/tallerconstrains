8a9e6752d97add592beb989a71be62aeb288d6d8
Fabian Monsalve <fgmonsalveb@unal.edu.co>
2015-10-20 23:18:44 -0500

    Creacion de el domain Post

---
+++ domyCons/grails-app/domain/domycons/Post.groovy
@@ -0,0 +1,22 @@
+package domycons
+
+class Post {
+
+    String topic // debe tener una longitud mínima de 3 caracteres y una  longitud máxima de 50 caracteres
+    Date dateCreated // La fecha de creación del post debe ser futura respecto a la fecha actual cuando se crea el post
+    Date lastUpdate // La fecha de actualización del post debe ser futura respecto a la fecha actual
+    boolean itsAllowed
+
+    static constraints = {
+        topic size: 3..50
+        dateCreated validator:{
+            Date dateobj = new Date();
+            if(!it.after(dateobj)) return ['la fecha es pasada a la creacion del post']
+        }
+        lastUpdate validator:{
+            Date dateobj = new Date();
+            if(!dateobj.after(it)) return ['la fecha de actualizacion es anterior a la fecha actual']
+        }
+
+    }
+}
