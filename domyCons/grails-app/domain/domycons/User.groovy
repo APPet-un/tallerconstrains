2f70016e8c429508715516650916fc38bd9bb8f7
Stephanie Torres <sttorresji@unal.edu.co>
2015-10-21 03:00:08 -0500

    Creation User class

---
+++ domyCons/grails-app/domain/domycons/User.groovy
@@ -0,0 +1,25 @@
+package domycons
+
+class User {
+
+    String name
+    String lastName
+    Integer age
+    String username
+    String password
+
+    static constraints = {
+        name(blank: false, nullable: false)
+        lastName(blank: false, nullable: false)
+        age(blank: false, nullable: false)
+        username(blank: false, nullable: false, unique: true)
+        password(blank: false, nullable: false)
+
+        name(size:3..50)
+        lastName(size:3..50)
+        age(min: 13)
+        password(matches: "[0-9a-za-z]+",min: 8)
+
+
+    }
+}
