a6fd2bd1a06c739e3ae226055777f33b4475741d
jhhfrancos <jhhfrancos@hotmail.com>
2015-10-21 23:30:30 -0500

    Configurations for test

--- domyCons/test/unit/domycons/PostSpec.groovy
+++ domyCons/test/unit/domycons/PostSpec.groovy
@@ -1,13 +1,16 @@
 package domycons
 
+import grails.test.mixin.TestFor
 import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import spock.lang.Specification
 
+import grails.util.Holders
+
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
-@TestMixin(GrailsUnitTestMixin)
+@TestFor(Post)
 class PostSpec extends Specification {
 
     def setup() {
--- domyCons/test/unit/domycons/UserSpec.groovy
+++ domyCons/test/unit/domycons/UserSpec.groovy
@@ -3,8 +3,10 @@ package domycons
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
+import grails.util.Holders
+
 /**
- * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
  */
 @TestFor(User)
 class UserSpec extends Specification {
